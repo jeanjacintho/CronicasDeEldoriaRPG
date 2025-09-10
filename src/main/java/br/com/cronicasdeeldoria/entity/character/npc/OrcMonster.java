dee6f3a8bd9a4f0d3ae5793e40be8ed08d7e6d90
mcalsing <marcelocalsing@gmail.com>
2025-09-09 21:14:07 -0300

    feat(monsterDrop): adicionando dois novos monstros: frostborn e orc

--- src/main/java/br/com/cronicasdeeldoria/entity/character/npc/OrcMonster.java
+++ src/main/java/br/com/cronicasdeeldoria/entity/character/npc/OrcMonster.java
@@ -1,9 +1,9 @@
 package br.com.cronicasdeeldoria.entity.character.npc;
 
-public class WolfMonster extends Npc {
+public class OrcMonster extends Npc {
   private int xpReward = 50;
   /**
-   * Cria um novo WolfMonster.
+   * Cria um novo OrcMonster.
    * @param name Nome do Monster.
    * @param isStatic Indica se o Monster é estático.
    * @param dialog Diálogo do Monster.
@@ -14,7 +14,8 @@ public class WolfMonster extends Npc {
    * @param interactive Indica se o Monster é interativo.
    * @param autoInteraction Indica se a interação é automática.
    */
-  public WolfMonster(String name, boolean isStatic, String dialog, int x, int y, String skin, int playerSize, boolean interactive, boolean autoInteraction) {
+
+  public OrcMonster(String name, boolean isStatic, String dialog, int x, int y, String skin, int playerSize, boolean interactive, boolean autoInteraction) {
     super(name, isStatic, dialog, x, y, skin, playerSize, interactive, autoInteraction);
   }
 
--- src/main/java/br/com/cronicasdeeldoria/entity/character/npc/SkeletonMonster.java
+++ src/main/java/br/com/cronicasdeeldoria/entity/character/npc/SkeletonMonster.java
@@ -4,14 +4,16 @@ public class SkeletonMonster extends Npc {
   private int xpReward = 30;
 
   /**
-   * Cria um novo OrcMonster.
-   * @param name Nome do NPC.
-   * @param isStatic Indica se o NPC é estático.
-   * @param dialog Diálogo do NPC.
+   * Cria um novo SkeletonMonster.
+   * @param name Nome do Monster.
+   * @param isStatic Indica se o Monster é estático.
+   * @param dialog Diálogo do Monster.
    * @param x Posição X.
    * @param y Posição Y.
-   * @param skin Skin do NPC.
+   * @param skin Skin do Monster.
    * @param playerSize Tamanho do jogador (para hitbox).
+   * @param interactive Indica se o Monster é interativo.
+   * @param autoInteraction Indica se a interação é automática.
    */
 
   public SkeletonMonster(String name, boolean isStatic, String dialog, int x, int y, String skin, int playerSize, boolean interactive, boolean autoInteraction) {
